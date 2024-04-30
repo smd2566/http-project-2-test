1cc805be35e9b3aae40ab7a958845b67c7bd6574
smd2566 <smd2566@rit.edu>
2024-04-29 21:00:17 -0400

    Initial file setup

---
+++ client/login.jsx
@@ -0,0 +1,125 @@
+const helper = require('./helper.js');
+const React = require('react');
+const {createRoot} = require('react-dom/client');
+
+const handleLogin = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const username = e.target.querySelector('#user').value;
+    const pass = e.target.querySelector('#pass').value;
+
+    if (!username || !pass) {
+        helper.handleError('Username or password is empty!');
+        return false;
+    }
+
+    helper.sendPost(e.target.action, {username, pass});
+    return false;
+}
+
+const handleSignup = (e) => {
+    e.preventDefault();
+    helper.hideError();
+
+    const username = e.target.querySelector('#user').value;
+    const pass = e.target.querySelector('#pass').value;
+    const pass2 = e.target.querySelector('#pass2').value;
+
+    if (!username || !pass || !pass2) {
+        helper.handleError('All fields are required!');
+        return false;
+    }
+
+    if (pass !== pass2) {
+        helper.handleError('Passwords do not match!');
+        return false;
+    }
+
+    helper.sendPost(e.target.action, {username, pass, pass2});
+
+    return false;
+}
+
+const LoginWindow = (props) => {
+    return(
+        <form id="loginForm"
+            name="loginForm"
+            onSubmit={handleLogin}
+            action="/login"
+            method="POST"
+            className="mainForm"
+        >
+            <label htmlFor="username">Username: </label>
+            <input id="user" type="text" name="username" placeholder="username" />
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password" />
+            <input className = "formSubmit" type="submit" value="Sign in" />
+        </form>
+    );
+};
+
+const SignupWindow = (props) => {
+    return(
+        <form id= "signupForm"
+            name="signupForm"
+            onSubmit={handleSignup}
+            action="/signup"
+            method="POST"
+            className="mainForm"
+        >
+            <label htmlFor="username">Username: </label>
+            <input id="user" type="text" name= "username" placeholder="username" />
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password" />
+            <label htmlFor="pass">Password: </label>
+            <input id="pass2" type="password" name="pass2" placeholder="retype password" />
+            <input className="formSubmit" type="submit" value="Sign up" />
+        </form>
+    );
+};
+
+
+const AboutWindow = (props) => {
+    return(
+        <form id= "aboutForm"
+            name="aboutForm"
+            className="mainForm"
+        >
+            <h2>A domo is a monster that you can create! Give it a name, an age and a level. Be sure to
+                create an account before doing so!</h2>
+        </form>
+    );
+};
+
+const init = () => {
+    const loginButton = document.getElementById('loginButton');
+    const signupButton = document.getElementById('signupButton');
+    const aboutButton = document.getElementById('aboutButton');
+
+    const root = createRoot(document.getElementById('content'));
+
+    loginButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        root.render( <LoginWindow /> );
+        return false;
+    });
+
+    signupButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        root.render( <SignupWindow />);
+        return false;
+    });
+
+    aboutButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        root.render( <AboutWindow />);
+        return false;
+    });
+
+    
+
+    root.render( <LoginWindow /> );
+};
+
+window.onload = init;
---
+++ server/controllers/Domo.js
@@ -0,0 +1,48 @@
+const models = require('../models');
+
+const { Domo } = models;
+
+const makerPage = async (req, res) => res.render('app');
+
+const makeDomo = async (req, res) => {
+  if (!req.body.name || !req.body.age || !req.body.level) {
+    return res.status(400).json({ error: 'Name, age and level are all required!' });
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    level: req.body.level,
+    owner: req.session.account._id,
+  };
+
+  try {
+    const newDomo = new Domo(domoData);
+    await newDomo.save();
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age, level: newDomo.level });
+  } catch (err) {
+    console.log(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists!' });
+    }
+    return res.status(500).json({ error: 'An error occured making domo!' });
+  }
+};
+
+const getDomos = async (req, res) => {
+  try {
+    const query = { owner: req.session.account._id };
+    const docs = await Domo.find(query).select('name age level').lean().exec();
+
+    return res.json({ domos: docs });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: 'Error retrieving domos!' });
+  }
+};
+
+module.exports = {
+  makerPage,
+  makeDomo,
+  getDomos,
+};
